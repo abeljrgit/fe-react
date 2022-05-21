95d85b242df320bd4c1b931b102de3277d07c467
LAPTOP-3A9U22R2\abelm <abelmanuellugtujr1996@gmail.com>
2022-05-21 19:30:08 +0800

    fe-react

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,39 @@
 import "./App.scss";
+import peopleMeeting from "../img/image-header-desktop.jpg";
 
 function App() {
-  return <main className="App">{/* Insert Your Code Here */}</main>;
+  return (
+    <section className="App">
+      <div className="card">
+        <div className="card__content">
+          <h1 className="card__lead">
+            Get <span>insights</span> that help your business grow.
+          </h1>
+          <p className="card__details">
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <div className="card__stats">
+            <div className="card__stats-item">
+              <span>10k+</span>
+              <span>COMPANIES</span>
+            </div>
+            <div className="card__stats-item">
+              <span>314</span>
+              <span>TEMPLATES</span>
+            </div>
+            <div className="card__stats-item">
+              <span>12M+</span>
+              <span>QUERIES</span>
+            </div>
+          </div>
+        </div>
+        <div className="card__thumbnail">
+          <img src={peopleMeeting} alt="People in meeting" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
