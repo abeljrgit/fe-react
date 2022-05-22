c9f63b8b04b26bc9d903f09698704296272eb833
LAPTOP-3A9U22R2\abelm <abelmanuellugtujr1996@gmail.com>
2022-05-22 11:14:17 +0800

    fe-react

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,40 @@
 import "./App.scss";
+import coverPhoto from "../img/bg-pattern-card.svg";
+import profilePhoto from "../img/image-victor.jpg";
 
 function App() {
-  return <main className="App">{/* Insert Your Code Here */}</main>;
+  return (
+    <section className="App">
+      <div className="card">
+        <div className="card__cover">
+          <img src={coverPhoto} alt="Cover Photo" />
+        </div>
+        <div className="card__profile">
+          <div className="card__profile-photo">
+            <img src={profilePhoto} alt="Profile Photo" />
+          </div>
+          <h1 className="card__profile-personal">
+            Victor Crest <span>26</span>
+          </h1>
+          <span className="card__profile-address">London</span>
+        </div>
+        <div className="card__stats">
+          <div className="card__stats-item">
+            <span>80K</span>
+            <span>Followers</span>
+          </div>
+          <div className="card__stats-item">
+            <span>803K</span>
+            <span>Likes</span>
+          </div>
+          <div className="card__stats-item">
+            <span>1.4K</span>
+            <span>Photos</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
